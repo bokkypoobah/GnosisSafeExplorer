aece1c0357669b4834ed604cd0d2de5768a14315
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2025-02-26 11:35:40 +1100

    Safe 1.1.1 mainnet addresses

---
+++ docs/addresses.js
@@ -0,0 +1,12 @@
+const ADDRESSES = {
+  "1.1.1": { // Safe version
+    1 : { // Mainnet
+      "create_and_add_modules": "0xF61A721642B0c0C8b334bA3763BA1326F53798C0",
+      "create_call": "0x8538FcBccba7f5303d2C679Fa5d7A629A8c9bf4A",
+      "default_callback_handler": "0xd5D82B6aDDc9027B22dCA772Aa68D5d74cdBdF44",
+      "gnosis_safe": "0x34CfAC646f301356fAa8B21e94227e3583Fe3F5F",
+      "multi_send": "0x8D29bE29923b68abfDD21e541b9374737B49cdAD",
+      "proxy_factory": "0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B",
+    },
+  },
+}
