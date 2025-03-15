0814630768d868bebac038aa33228714f1d9a53b
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2025-03-16 08:39:04 +1100

    start of rewrite

---
+++ new_docs/globals.js
@@ -0,0 +1,14 @@
+const ADDRESS0 = "0x0000000000000000000000000000000000000000";
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
+
+function now() {
+  return moment().format("HH:mm:ss");
+}
